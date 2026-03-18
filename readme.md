840e99c7bf5878bb54d36d54c7883af085866b9d
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-03-18 15:41:37 +0700

    Update U-04
    có readme

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
+# Migration SQL (user + user_otps + refresh_tokens)
+001_create_user_tables.sql
+
 **Search Flight**
 
-# Migration SQL (tạo bảng + seed data mẫu)
+# Migration SQL (tạo bảng cho flight + seed vài data mẫu)
 002_create_flight_tables.sql
 # Flight Service (logic search)
 flight.service.js
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
+const flightRoutes = require("./routes/flight.routes");
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/flights", flightRoutes);
 
 module.exports = app;
