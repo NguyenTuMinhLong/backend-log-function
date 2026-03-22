199201f22507c16fffde1a65dacceaa99f06260c
NguyenTuMinhLong <nguyentuminhlong020700@gmail.com>
2026-03-22 12:24:32 +0700

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -9,36 +9,68 @@ const sendOTPEmail = async (to, otp) => {
       to: to,
       subject: "Your OTP Code",
       html: `
-        <div style="
-          max-width:400px;
-          margin:auto;
-          padding:20px;
-          font-family:sans-serif;
-          text-align:center;
-          border:1px solid #eee;
-          border-radius:12px;
-        ">
-          <h2 style="color:#1a73e8;">✈️ Vivudee</h2>
-          
-          <p style="color:#555;">
-            Your verification code is:
-          </p>
+  <div style="
+    background:#f5f7fb;
+    padding:30px 0;
+    font-family:Arial, sans-serif;
+  ">
+    <div style="
+      max-width:420px;
+      margin:auto;
+      background:#ffffff;
+      padding:24px;
+      border-radius:12px;
+      border:1px solid #eaeaea;
+      box-shadow:0 4px 12px rgba(0,0,0,0.08);
+      text-align:center;
+    ">
+      <img src="https://i.postimg.cc/gJfMDp49/Logo-Nav.jpg" width="70" style="margin-bottom:10px;" />
 
-          <div style="
-            font-size:32px;
-            font-weight:bold;
-            letter-spacing:4px;
-            color:#111;
-            margin:20px 0;
-          ">
-            ${otp}
-          </div>
+      
+      <p style="
+        color:#555;
+        font-size:14px;
+      ">
+        Your verification code is:
+      </p>
 
-          <p style="color:#999; font-size:12px;">
-            This code will expire in 5 minutes.
-          </p>
-        </div>
-      `,
+      
+      <div style="
+        font-size:34px;
+        font-weight:bold;
+        letter-spacing:8px;
+        margin:20px 0;
+        color:#111;
+      ">
+        ${otp}
+      </div>
+
+      
+      <p style="
+        color:#888;
+        font-size:13px;
+      ">
+        This code will expire in 5 minutes.
+      </p>
+
+      <!-- Divider -->
+      <div style="
+        height:1px;
+        background:#eee;
+        margin:20px 0;
+      "></div>
+
+      <!-- Footer -->
+      <p style="
+        font-size:12px;
+        color:#aaa;
+      ">
+        If you didn’t request this, please ignore this email.
+      </p>
+
+    </div>
+  </div>
+`,
     });
 
     if (error) {
