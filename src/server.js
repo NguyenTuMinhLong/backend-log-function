0cac8d9a4632c650e71e5a145ca12d76cabffe00
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-03-25 16:45:35 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Kết nối PostgreSQL thành công!`);
-  console.log(`Server running on port ${PORT}`);
+  console.log(`[Kết nối thành công] Server đang chạy trên "PORT: ${PORT}"`);
 });
